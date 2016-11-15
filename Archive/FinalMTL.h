059d7eed878983451a1d77c092b7575b0718e1ef
Tony <tony1991417@hotmail.com>
2016-11-14 20:11:31 -0800

    3DCorbal

---
+++ Archive/FinalMTL.h
@@ -0,0 +1,41 @@
+// Created with mtl2opengl.pl
+
+/*
+source files: ./Final.obj, ./Final.mtl
+materials: 1
+
+Name: defaultMat
+Ka: 0.000, 0.000, 0.000
+Kd: 0.000, 0.000, 0.000
+Ks: 0.500, 0.500, 0.500
+Ns: 96.078
+
+*/
+
+
+int FinalMTLNumMaterials = 1;
+
+int FinalMTLFirst [1] = {
+0,
+};
+
+int FinalMTLCount [1] = {
+209988,
+};
+
+float FinalMTLAmbient [1][3] = {
+0.000,0.000,0.000,
+};
+
+float FinalMTLDiffuse [1][3] = {
+0.000,0.000,0.000,
+};
+
+float FinalMTLSpecular [1][3] = {
+0.500,0.500,0.500,
+};
+
+float FinalMTLExponent [1] = {
+96.078,
+};
+
